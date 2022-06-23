da6b6dc86f97be3298ca29c235cd7a0e38f25da8
Sterea Victor <sterea.victor@gmail.com>
2022-06-23 17:22:03 +0300

    user repository dependency injection

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,11 @@
 import { Injectable } from "@nestjs/common";
 
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+
 @Injectable()
 export class UsersService {
+  constructor(@InjectRepository(User) userRepository: Repository<User>) { }
 
   findAll(): void {
     console.log("Merge");
