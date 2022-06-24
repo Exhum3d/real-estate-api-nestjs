4b0522ad3fc6979433dbb9cbde020519811771eb
Sterea Victor <sterea.victor@gmail.com>
2022-06-24 13:06:39 +0300

    apply validations on User entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
 import { Exclude } from "class-transformer";
+import { IsBoolean, IsEmail, IsString, MaxLength } from "class-validator";
 import { Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
 
 
@@ -8,21 +9,32 @@ export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @IsString()
+  @MaxLength(30)
   @Column()
   firstName: string;
 
+  @IsString()
+  @MaxLength(30)
   @Column()
   lastName: string;
 
+  @IsEmail()
   @Column()
   email: string;
 
+
+  @IsString()
+  @MaxLength(30)
+  @Column()
   phone: string;
 
+  @IsString()
   @Column()
   @Exclude()
   password: string;
 
+  @IsBoolean()
   @Column('boolean', { default: false })
   isAdmin: boolean;
 }
